dba4b995bb4d2a9f027df4f1617b96da3dfcb974
Mosazghi <fest121@hotmail.com>
2026-02-09 11:11:04 +0100

    WIP on task 3

--- pre-labs/pre_1.m
+++ pre-labs/pre_1.m
@@ -4,11 +4,25 @@
 
 %% Initialization and model definition
 initXX; % Change this to the init file corresponding to your helicopter
+Ac = [ 0      1        0          0;
+       0      0      -K_2        0;
+       0      0        0          1;
+       0      0   -K_1*K_pp  -K_1*K_pd ];
+
+Bc = [0;
+      0;
+      0;
+      K_1*K_pp];
 
 % Discrete time system model. x = [lambda r p p_dot]'
 delta_t	= 0.25; % sampling time
-A1 = [];
-B1 = [];
+sysc = ss(Ac,Bc,[],[]);          % C,D can be [] or zeros
+% Discretize with zero‑order hold
+sysd = c2d(sysc,delta_t,'zoh');  % or 'tustin', 'foh', etc.
+
+% Get Ad and Bd
+A1= sysd.A;
+B1 = sysd.B;
 
 % Number of states and inputs
 mx = size(A1,2); % Number of states (number of columns in A)
@@ -16,20 +30,20 @@ mu = size(B1,2); % Number of inputs(number of columns in B)
 
 % Initial values
 x1_0 = pi;                               % Lambda
-x2_0 = ;                               % r
-x3_0 = ;                               % p
-x4_0 = ;                               % p_dot
+x2_0 = 0;                               % r
+x3_0 = 0;                               % p
+x4_0 = 0;                               % p_dot
 x0 = [x1_0 x2_0 x3_0 x4_0]';           % Initial values
 
 % Time horizon and initialization
-N  = ;                                  % Time horizon for states
+N  = 100;                                  % Time horizon for states
 M  = N;                                 % Time horizon for inputs
 z  = zeros(N*mx+M*mu,1);                % Initialize z for the whole horizon
 z0 = z;                                 % Initial value for optimization
 
 % Bounds
-ul 	    = ;                   % Lower bound on control
-uu 	    = ;                   % Upper bound on control
+ul 	    = [0 0 0 0];                      % Lower bound on control
+uu 	    = [0 0 60*pi/360 0];              % Upper bound on control
 
 xl      = -Inf*ones(mx,1);              % Lower bound on states (no bound)
 xu      = Inf*ones(mx,1);               % Upper bound on states (no bound)
@@ -37,27 +51,27 @@ xl(3)   = ul;                           % Lower bound on state x3
 xu(3)   = uu;                           % Upper bound on state x3
 
 % Generate constraints on measurements and inputs
-[vlb,vub]       = ; % hint: gen_constraints
+[vlb,vub]       = gen_constraints(N, M, xl, xu, ul, uu); % hint: gen_constraints
 vlb(N*mx+M*mu)  = 0;                    % We want the last input to be zero
 vub(N*mx+M*mu)  = 0;                    % We want the last input to be zero
 
 % Generate the matrix Q and the vector c (objecitve function weights in the QP problem) 
 Q1 = zeros(mx,mx);
-Q1(1,1) = ;                            % Weight on state x1
-Q1(2,2) = ;                            % Weight on state x2
-Q1(3,3) = ;                            % Weight on state x3
-Q1(4,4) = ;                            % Weight on state x4
+Q1(1,1) = 0;                            % Weight on state x1
+Q1(2,2) = 0;                            % Weight on state x2
+Q1(3,3) = 0.12;                            % Weight on state x3
+Q1(4,4) = 0;                            % Weight on state x4
 P1 = 0;                                % Weight on input
-Q = ;                                  % Generate Q, hint: gen_q
+Q = gen_q(Q1, P1, N, M);                                  % Generate Q, hint: gen_q
 c = ;                                  % Generate c, this is the linear constant term in the QP
 
 %% Generate system matrixes for linear model
-Aeq = ;             % Generate A, hint: gen_aeq
+Aeq = gen_aeq   (A1, B1, N, mx, mu);             % Generate A, hint: gen_aeq
 beq = ;             % Generate b
 
 %% Solve QP problem with linear model
 tic
-[z,lambda] = ; % hint: quadprog. Type 'doc quadprog' for more info 
+[z,lambda] = quadprog(H, f); % hint: quadprog. Type 'doc quadprog' for more info 
 t1=toc;
 
 % Calculate objective value
